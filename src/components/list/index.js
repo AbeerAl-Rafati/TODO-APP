642acf82585d4d44525e0faaf1b149790ebeba5c
abeeral-rafati <abeerw.rafati@yahoo.com>
2021-08-16 07:54:08 +0300

    refactoring

---
+++ src/components/list/index.js
@@ -0,0 +1,59 @@
+// import React, { Component } from 'react'
+
+// export class List extends Component {
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//     toggleComplete: props.toggleComplete,
+//     list : props.list
+//   }
+// }
+
+//   render() {
+//     return (
+//       this.state.list.map((item) => (
+//               <div key={item.id}>
+//                 <p>{item.text}</p>
+//                 <p>
+//                   <small>Assigned to: {item.assignee}</small>
+//                 </p>
+//                 <p>
+//                   <small>Difficulty: {item.difficulty}</small>
+//                 </p>
+//                 <div onClick={() => this.state.toggleComplete(item.id)}>
+//                   Complete: {item.complete.toString()}
+//                 </div>
+//                 <hr />
+//               </div>
+//             ))
+//     )
+//   }
+// }
+
+// export default List
+
+
+function List(props) {
+  return (
+  
+    props.list.map((item) => (
+      <div key={item.id}>
+        <p>{item.text}</p>
+        <p>
+          <small>Assigned to: {item.assignee}</small>
+        </p>
+        <p>
+          <small>Difficulty: {item.difficulty}</small>
+        </p>
+        <div onClick={() => props.toggleComplete(item.id)}>
+          Complete: {item.complete.toString()}
+        </div>
+        <hr />
+      </div>
+    ))
+  
+  )
+}
+
+export default List
